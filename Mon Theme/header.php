4b30c477b08dd06173b422c27348c4b79e961a97
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-03-22 15:03:02 +0100

    Update header.php

--- Mon Theme/header.php
+++ Mon Theme/header.php
@@ -4,9 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" , shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <?php
+    if (is_front_page()) :
+    ?>
+        <title>Accueil | <?php bloginfo('name'); ?></title>
+    <?php
+    else :
+    ?>
+        <title><?php wp_title(); ?> | <?php bloginfo('name'); ?></title>
+    <?php
+    endif;
+    ?>
+    <link rel="shortcut icon" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/img/icons8-bulle.svg" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
     <?php wp_head() ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
