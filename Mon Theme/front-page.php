bdd664f045ff5f4a83bc8291ae441977dc8975a6
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-03-24 16:22:04 +0100

    Update front-page.php

--- Mon Theme/front-page.php
+++ Mon Theme/front-page.php
@@ -1,6 +1,7 @@
 <?php
-
+if (!defined('ABSPATH')) exit;
 get_header();
+
 
 // Affichage de la page d'accueil sans la boucle
 if (have_posts()) :
@@ -9,7 +10,9 @@ if (have_posts()) :
     <article>
         <?php // Notez que le titre est dans un lien clickable 
         ?>
+
         <h2><a href="<?= get_permalink(); ?>"><?php the_title(); ?></a></h2>
+
         <div><?php the_time(get_option('date_format')); ?></div>
         <?php
         if (has_post_thumbnail()) :
@@ -19,7 +22,8 @@ if (have_posts()) :
         <?php
         endif;
         ?>
-        <div><?php the_content(); ?></div>
+
+        <div><?php the_content('en voir plus'); ?></div>
     </article>
 <?php
 endif;
