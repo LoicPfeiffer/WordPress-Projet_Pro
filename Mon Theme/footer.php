63ff45a540131ebb3fcb393215508bece8dffd56
LoicPfeiffer <95574385+LoicPfeiffer@users.noreply.github.com>
2022-03-24 16:21:36 +0100

    Update footer.php

--- Mon Theme/footer.php
+++ Mon Theme/footer.php
@@ -1,4 +1,14 @@
+</div>
 <?php wp_footer() ?>
+<footer>
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'footer',
+        'menu' => 'footer-menu',
+        'container_class' => 'custom-menu-class-footer',
+    ));
+    ?>
+</footer>
 </body>
 
 </html>
